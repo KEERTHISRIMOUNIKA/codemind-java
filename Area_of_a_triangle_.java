62043992bdc543079eeb3f25bb4816fd5033c7e5
KEERTHISRIMOUNIKA <147792226+KEERTHISRIMOUNIKA@users.noreply.github.com>
2024-07-31 22:02:23 +0530

    import java.util.Scanner; public class area {     public static void main(String[] args)     {         int a,b,c;         Scanner read = new Scanner(System.in);         a = read.nextInt();         b = read.nextInt();         c = (a*b)/2;         System.out.println(c);     } }

---
+++ Area_of_a_triangle_.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class area
+{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        Scanner read = new Scanner(System.in);
+        a = read.nextInt();
+        b = read.nextInt();
+        c = (a*b)/2;
+        System.out.println(c);
+    }
+}
