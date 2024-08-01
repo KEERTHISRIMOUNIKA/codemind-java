9dd3ba3884db315d3e1f80f646aeb57756cde667
KEERTHISRIMOUNIKA <147792226+KEERTHISRIMOUNIKA@users.noreply.github.com>
2024-08-01 19:56:19 +0530

    import java.util.Scanner; public class travel {     public static void main(String[] args)     {         int N,M,R;         Scanner read = new Scanner(System.in);         N = read.nextInt();         M = read.nextInt();         R=(5*N)+(7*M);         System.out.println(R);
        }
    }

---
+++ King_Tours_.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class travel
+{
+    public static void main(String[] args)
+    {
+        int N,M,R;
+        Scanner read = new Scanner(System.in);
+        N = read.nextInt();
+        M = read.nextInt();
+        R=(5*N)+(7*M);
+        System.out.println(R);
+        
+    }
+}
