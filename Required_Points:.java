6341e6422e69d84a302319ec78e878f75f4e7cdc
KEERTHISRIMOUNIKA <147792226+KEERTHISRIMOUNIKA@users.noreply.github.com>
2024-08-02 21:33:10 +0530

    import java.util.Scanner; public class demo {     public static void main(String[] args)
         { 
           int x,y,z;
           Scanner read = new Scanner(System.in);
           x = read.nextInt();
           y = read.nextInt();
           z = y/x;
           System.out.println(z);
         }
        
        
    }

---
+++ Required_Points:.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class demo
+{
+    public static void main(String[] args)
+      
+     { 
+       int x,y,z;
+       Scanner read = new Scanner(System.in);
+       x = read.nextInt();
+       y = read.nextInt();
+       z = y/x;
+       System.out.println(z);
+     }
+    
+    
+}
